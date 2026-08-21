45edd9166e53db4976f732f970e16376490e18b2
Drownedpeppa <iusejcnotac@gmail.com>
2026-08-21 06:56:23 -0400

    idle parkour void update

--- scenes/game/idle_parkour/idle_parkour.gd
+++ scenes/game/idle_parkour/idle_parkour.gd
@@ -3,6 +3,8 @@ extends Node3D
 @onready var blink: ColorRect = $player_controller/view/blink
 @onready var todo: Label = $player_controller/view/todo
 @onready var parkour_points: Label = $player_controller/view/parkour_points
+@onready var player: player_controller = $player_controller
+@onready var spawn_point: Node3D = $start_platform/spawn_point
 
 func _ready() -> void:
 	blink.visible = false
@@ -17,5 +19,8 @@ func _exit_tree() -> void:
 
 func _on_return_body_entered(body: Node3D) -> void:
 	if body is player_controller:
-		get_tree().change_scene_to_file(PlayerStats.current_scene)
+		if PlayerStats.current_scene != "":
+			get_tree().change_scene_to_file(PlayerStats.current_scene)
+		else:
+			player.global_position = spawn_point.global_position
 		
